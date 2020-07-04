ff2f9d30abb12db26e6790f18ee0fcccb84a151a
Ana <analucia.spi02@gmail.com>
2020-07-03 22:05:43 -0400

    add exercises 1.3 to 1.5

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -2,17 +2,29 @@ import React from 'react'
 import Part from './Part'
 
 const Content = props => {
-const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
     return (
         <>
-           <Part part1 = {part1} exercise1 ={exercises1}/>
-           <Part part1 = {part2} exercise2 ={exercises2}/>
-           <Part part3 = {part3} exercise3 ={exercises3}/> 
+           <Part part1 = {course.parts[0].name} exercise1 ={course.parts[0].exercises}/>
+           <Part part2 = {course.parts[1].name} exercise2 ={course.parts[0].exercises}/>
+           <Part part3 = {course.parts[2].name} exercise3 ={course.parts[0].exercises}/> 
         </>
     )
 }
